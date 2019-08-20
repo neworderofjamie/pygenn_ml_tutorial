6a5d31ad04dd5764be59f5cd4064de808fed4644
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-08-20 15:19:26 +0100

    working example

--- spiking_eval.py
+++ spiking_eval.py
@@ -1,25 +1,24 @@
 import numpy as np
 from os import path
-import matplotlib.pyplot as plt
-from pygenn import genn_model, genn_wrapper
 
-def record_current_spikes(pop, spikes, dt):
-    current_spikes = pop.current_spikes
-    current_spike_times = np.ones(current_spikes.shape) * dt
+from pygenn.genn_model import (create_custom_neuron_class,
+                               create_custom_current_source_class,
+                               GeNNModel)
+from pygenn.genn_wrapper import NO_DELAY
 
-    if spikes is None:
-        return (np.copy(current_spikes), current_spike_times)
-    else:
-        return (np.hstack((spikes[0], current_spikes)),
-                np.hstack((spikes[1], current_spike_times)))
-
+# ----------------------------------------------------------------------------
+# Parameters
+# ----------------------------------------------------------------------------
 IF_PARAMS = {"Vthr": 5.0}
 TIMESTEP = 1.0
-PRESENT_TIMESTEPS = 350
+PRESENT_TIMESTEPS = 100
 INPUT_CURRENT_SCALE = 1.0 / 100.0
 
-# Custom classes
-if_model = genn_model.create_custom_neuron_class(
+# ----------------------------------------------------------------------------
+# Custom GeNN models
+# ----------------------------------------------------------------------------
+# Very simple integrate-and-fire neuron model
+if_model = create_custom_neuron_class(
     "if_model",
     param_names=["Vthr"],
     var_name_types=[("V", "scalar"), ("SpikeCount", "unsigned int")],
@@ -28,16 +27,19 @@ if_model = genn_model.create_custom_neuron_class(
     $(V) = 0.0;
     $(SpikeCount)++;
     """,
-    threshold_condition_code="$(V) >= $(Vthr)"
-)
+    threshold_condition_code="$(V) >= $(Vthr)")
 
-cs_model = genn_model.create_custom_current_source_class(
+# Current source model which injects current with a magnitude specified by a state variable
+cs_model = create_custom_current_source_class(
     "cs_model",
     var_name_types=[("magnitude", "scalar")],
     injection_code="$(injectCurrent, $(magnitude));")
 
+# ----------------------------------------------------------------------------
+# Build model
+# ----------------------------------------------------------------------------
 # Create GeNN model
-model = genn_model.GeNNModel("float", "spiking_eval")
+model = GeNNModel("float", "spiking_eval")
 model.dT = TIMESTEP
 
 # Load weights
@@ -46,42 +48,41 @@ while True:
     filename = "weights_%u_%u.npy" % (len(weights), len(weights) + 1)
     if path.exists(filename):
         weights.append(np.load(filename))
-
-        if len(weights) >= 2:
-            assert weights[-1].shape[0] == weights[-2].shape[1]
     else:
         break
-
-print("Loaded %u weights" % len(weights))
 
 # Initial values to initialise all neurons to
 if_init = {"V": 0.0, "SpikeCount":0}
 
-
 # Create first neuron layer
-neuron_layers = [model.add_neuron_population("neuron0", weights[0].shape[0], if_model, IF_PARAMS, if_init)]
+neuron_layers = [model.add_neuron_population("neuron0", weights[0].shape[0],
+                                             if_model, IF_PARAMS, if_init)]
 
 # Create subsequent neuron layer
 for i, w in enumerate(weights):
-    neuron_layers.append(model.add_neuron_population("neuron%u" % (i + 1), w.shape[1], if_model, IF_PARAMS, if_init))
-
-print("Created %u neuron layers" % len(neuron_layers))
+    neuron_layers.append(model.add_neuron_population("neuron%u" % (i + 1),
+                                                     w.shape[1], if_model,
+                                                     IF_PARAMS, if_init))
 
 # Create synaptic connections between layers
 for i, (pre, post, w) in enumerate(zip(neuron_layers[:-1], neuron_layers[1:], weights)):
     model.add_synapse_population(
-        "synapse%u" % i, "DENSE_INDIVIDUALG", genn_wrapper.NO_DELAY,
+        "synapse%u" % i, "DENSE_INDIVIDUALG", NO_DELAY,
         pre, post,
-        "StaticPulse", {}, {"g": w.flatten(-1)}, {}, {},
+        "StaticPulse", {}, {"g": w.flatten()}, {}, {},
         "DeltaCurr", {}, {})
 
 # Create current source to deliver input to first layers of neurons
-current_input = model.add_current_source("current_input", cs_model, "neuron0" , {}, {"magnitude": 0.0})
+current_input = model.add_current_source("current_input", cs_model,
+                                         "neuron0" , {}, {"magnitude": 0.0})
 
 # Build and load our model
 model.build()
 model.load()
 
+# ----------------------------------------------------------------------------
+# Simulate
+# ----------------------------------------------------------------------------
 # Load testing data
 testing_images = np.load("testing_images.npy")
 testing_labels = np.load("testing_labels.npy")
@@ -90,33 +91,57 @@ testing_labels = np.load("testing_labels.npy")
 assert testing_images.shape[1] == weights[0].shape[0]
 assert np.max(testing_labels) == (weights[1].shape[1] - 1)
 '''
-# Get views to efficiently access state variables
-current_input_magnitude = current_input.vars["magnitude"].view
+# Set current input by scaling first image
+current_input.vars["magnitude"].view[:] = testing_images[0] * INPUT_CURRENT_SCALE
 
-print(testing_labels[5])
+# Upload
+model.push_var_to_device("current_input", "magnitude")
+
 layer_spikes = [None] * len(neuron_layers)
 while model.timestep < PRESENT_TIMESTEPS:
-    # If this is the first timestep of the presentation
-    if model.timestep == 0:
-        # Show first training image
-        current_input_magnitude[:] = testing_images[5] * INPUT_CURRENT_SCALE
-
-        # Upload
-        model.push_var_to_device("current_input", "magnitude")
-
     # Advance simulation
     model.step_time()
 
+    # Loop through neuron layers
     for i, l in enumerate(neuron_layers):
+        # Download spikes
         model.pull_current_spikes_from_device(l.name)
-        layer_spikes[i] = record_current_spikes(l, layer_spikes[i], model.t)
 
+        # Add to data structure
+        spike_times = np.ones_like(l.current_spikes) * model.t
+        if layer_spikes[i] is None:
+            layer_spikes[i] = (np.copy(l.current_spikes), spike_times)
+        else:
+            layer_spikes[i] = (np.hstack((layer_spikes[i][0], l.current_spikes)),
+                               np.hstack((layer_spikes[i][1], spike_times)))
 
+# ----------------------------------------------------------------------------
+# Plotting
+# ----------------------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+# Create a plot with axes for each
 fig, axes = plt.subplots(len(neuron_layers), sharex=True)
 
-for a, s in zip(axes, layer_spikes):
+
+# Loop through axes and their corresponding neuron populations
+for a, s, l in zip(axes, layer_spikes, neuron_layers):
+    # Plot spikes
     a.scatter(s[1], s[0], s=1)
 
+    # Set title, axis labels
+    a.set_title(l.name)
+    a.set_ylabel("Spike number")
+    a.set_xlim((0, PRESENT_TIMESTEPS * TIMESTEP))
+    a.set_ylim((0, l.size))
+
+
+# Add an x-axis label and translucent line showing the correct label
+axes[-1].set_xlabel("Time [ms]")
+axes[-1].hlines(testing_labels[0], xmin=0, xmax=PRESENT_TIMESTEPS,
+                linestyle="--", color="gray", alpha=0.2)
+
+# Show plot
 plt.show()
 '''
 # Get views to efficiently access state variables
@@ -124,18 +149,24 @@ current_input_magnitude = current_input.vars["magnitude"].view
 output_spike_count = neuron_layers[-1].vars["SpikeCount"].view
 layer_voltages = [l.vars["V"].view for l in neuron_layers]
 
-while model.timestep < (PRESENT_TIMESTEPS * 1000):
+# Simulate
+num_correct = 0
+while model.timestep < (PRESENT_TIMESTEPS * testing_images.shape[0]):
     # Calculate the timestep within the presentation
     timestep_in_example = model.timestep % PRESENT_TIMESTEPS
     example = int(model.timestep // PRESENT_TIMESTEPS)
 
-    # If this is the first timestep of the presentation
+    # If this is the first timestep of presenting the example
     if timestep_in_example == 0:
         current_input_magnitude[:] = testing_images[example] * INPUT_CURRENT_SCALE
         model.push_var_to_device("current_input", "magnitude")
 
+        # Loop through all layers and their corresponding voltage views
         for l, v in zip(neuron_layers, layer_voltages):
+            # Manually 'reset' voltage
             v[:] = 0.0
+
+            # Upload
             model.push_var_to_device(l.name, "V")
 
         # Zero spike count
@@ -145,11 +176,20 @@ while model.timestep < (PRESENT_TIMESTEPS * 1000):
     # Advance simulation
     model.step_time()
 
+    # If this is the LAST timestep of presenting the example
     if timestep_in_example == (PRESENT_TIMESTEPS - 1):
+        # Download spike count from last layer
         model.pull_var_from_device(neuron_layers[-1].name, "SpikeCount")
 
-        most_spikes = np.argmax(output_spike_count)
-        if most_spikes == testing_labels[example]:
-            print("CORRECT!")
-        else:
-            print("INCORRECT!")
+        # Find which neuron spiked the most to get prediction
+        predicted_label = np.argmax(output_spike_count)
+        true_label = testing_labels[example]
+
+        print("\tExample=%u, true label=%u, predicted label=%u" % (example,
+                                                                   true_label,
+                                                                   predicted_label))
+
+        if predicted_label == true_label:
+            num_correct += 1
+
+print("Accuracy %f%%" % ((num_correct / float(testing_images.shape[0])) * 100.0))
